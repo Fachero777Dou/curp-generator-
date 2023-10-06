8a208852966864c93c1833483e8f1ba75f0a5886
marcmartinez <marcmartinez@DESKTOP-PEBA1BE.carsdirect.win>
2023-10-05 17:54:17 -0700

    Generador de curp

---
+++ src/main/java/mx/itson/curpgenerator/curp/Procedure.java
@@ -0,0 +1,19 @@
+
+package mx.itson.curpgenerator.curp;
+
+/**
+ *Contiene métodos para generar la curp
+ * @author Sergio Alejando, Yolanda Martinez, Luis Daniel, Jose Alvarez, Pedro 
+ */
+public class Procedure {
+    
+    public String people(String name, String firstName, String lastName, String date, String sex){
+    
+    }
+    
+    
+    
+    
+    
+    
+}
