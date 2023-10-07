89fe97977ade2e92d21cb53fbcb6cbf11670dc96
marcmartinez <marcmartinez@DESKTOP-PEBA1BE.carsdirect.win>
2023-10-07 09:03:34 -0700

    Se actualizo el método prueba 2

--- src/main/java/mx/itson/curpgenerator/curp/Procedure.java
+++ src/main/java/mx/itson/curpgenerator/curp/Procedure.java
@@ -11,7 +11,12 @@ public class Procedure {
     public String people(String name, String firstName, String lastName, String date, String sex){
     
     }
+    
+    
+    poniendo a prueba 
 */
+    String name;
+    String firstNAME;
     
     
     
