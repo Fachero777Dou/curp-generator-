aa7684950388b9b827e4bd1ac8d717047bf5b0b4
marcmartinez <marcmartinez@DESKTOP-PEBA1BE.carsdirect.win>
2023-10-08 12:50:11 -0700

    Se agrego comentarios y se agrego un if name

--- src/main/java/mx/itson/curpgenerator/curp/Procedure.java
+++ src/main/java/mx/itson/curpgenerator/curp/Procedure.java
@@ -9,11 +9,32 @@ public class Procedure {
 
     public String people(String name, String firstName, String lastName, String date, String sex) {
 
-        //ejemplo.... nomas de ejmplo
-        if (name.equals(name)) {
-            System.out.println("Soyy.....");
-        }
-        return name;
+      //de ejemplo usare el repositorio de sergio
+      //Van a descargar primero el archivo ahi donde señale
+      //luego la van a clonar, de la siguiente manera 
+      // se iran donde se descargo el archivo en mi caso es en el escritorio
+      /*para clonar le dan click derecho y Open Git bahs here 
+       despues en la consola pondrán git clone https://github.com/Fachero777Dou/curp-generator-.git la liga del proyecto 
+      */
+      
+      /*
+      
+      despues de clonar se vendrán aqui a neatbeans y harán lo siguiente 
+      primero le ponen inicializar repositorio 
+      despues se iran a pull para bajar el proyecto 
+      despues del pull empiezan a trabajar en el proyecto
+      */
+      
+      
+      //ejemplo es solo un ejemplo
+      /*
+      después de trabajar en el hacen lo siguiente 
+      */
+      
+      if(name.equals(name)){
+          System.out.println("name");
+      }
+      return name;
 
     }
 
